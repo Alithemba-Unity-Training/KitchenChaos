604e3702317ba8f029c4d771f786fea96d0e9ae2
Alithemba Makongwana <a.makongwana@gmail.com>
2024-07-31 04:21:36 +0200

    Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool isWalking;
+
     [SerializeField] private float moveSpeed = 7f;
     private void Update()
     {
@@ -34,6 +36,16 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-        Debug.Log(Time.deltaTime);
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+               
+    }
+
+    public bool IsWalking() 
+    {
+        return isWalking;
     }
 }
